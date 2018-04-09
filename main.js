927b10a79542bb1fb838893a1d329173abd9d928
Rich T <rtranter25@gmail.com>
2018-04-09 16:00:43 -0500

    adding comments to the code

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+
+// creating an array of objects
 var animals = [
 {name: "rich", species: "dog"},
 {name: "chone", species: "cat"},
 {name: "john", species: "cat"}
 ]
 
+// making an empty array for temporary 
 var cats = [];
 
+// creating a function to loop through the animals array
 function findDog() {
 	for (i = 0; i < animals.length; i++){
 		if (animals[i].species == "cat"){
